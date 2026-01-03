53c3e9acfd06d9adf58ad439dd3882ae0cbb32e9
Mohamed-Eddib <dahmouneeddib@gmail.com>
2026-01-03 13:56:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const LightDarkMode = document.getElementById("light-dark-mode");
-const DarkLightSwitch = document.getElementById("light-dark-icon");
-LightDarkMode.onclick = () => {
-       document.documentElement.style.setProperty('--key-color', 'hsl(0, 0%, 0%)');
-       document.documentElement.style.setProperty('--text-color', 'hsl(0, 100%, 100%)');
-    }
-    
+
